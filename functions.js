ea9dd51bdc656ff66683370757cec71ee0bca09c
Zayon Nieves <zayon.nieves1@gmail.com>
2022-12-09 20:50:12 -0500

    export functions for use in other files

--- functions.js
+++ functions.js
@@ -4,7 +4,7 @@ export to main project file*/
 
 
 // Step #3: asking for input 3 times. Declare a function to re-use for this
-export function getInput(question) {
+ function getInput(question) {
 
     // prompt-sync for user input through console
     const input = require('prompt-sync')();
@@ -19,7 +19,7 @@ export function getInput(question) {
             continue;
         }
         // check input (unit)
-        else if ((question.includes('unit')) && (userInput !== 'F' || userInput !== 'C' || userInput !== 'K')  ) {
+        else if ((question.includes('unit')) && (userInput !== 'F' && userInput !== 'C' && userInput !== 'K')  ) {
             console.log('Input for unit is invalid. Try again.');
             continue;
         }
@@ -33,8 +33,71 @@ export function getInput(question) {
 
 
 // Function: process input and produce the output to display to user
-export function tempConversion(initialTemp, initialUnit, finalUnit) {
+ function tempConversion(initialTemp, initialUnit, finalUnit) {
 
     // figure out which formula to use based on the inputs
 
+    // initial and final unit are the same. No action to perform
+    if(initialUnit === finalUnit) {
+        return initialTemp;
+    }
+    // If starting unit is Fahrenheit
+    else if(initialUnit === 'F') {
+        return fahrenheitTo(initialTemp, finalUnit);
+    }
+    // If starting unit is Celsius
+    else if(initialUnit === 'C') {
+        return celsiusTo(initialTemp, finalUnit);
+    }
+    // If starting unit is Kelvin
+    else {
+        return kelvinTo(initialTemp, finalUnit);
+    }
+
 }
+
+
+// Helper Functions for tempConversion function
+
+// converting from fahrenheit
+function fahrenheitTo(startingTemp, finalUnit) {
+
+    // Fahrenheit to Celsius
+    if(finalUnit === 'C') {
+        return (startingTemp - 32) * (5/9);
+    }
+    // Fahrenheit to Kelvin
+    else {
+        return (startingTemp-32) * (5/9) + 273.15;
+    }
+}
+
+// converting from celsius
+function celsiusTo(startingTemp, finalUnit) {
+
+    // Celsius to Fahrenheit
+    if(finalUnit === 'F') {
+        return (startingTemp * (9/5) + 32)
+    }
+    // Celsius to Kelvin
+    else {
+        return (Number(startingTemp) + 273.15);
+    }
+}
+
+// converting from kelvin
+function kelvinTo(startingTemp, finalUnit) {
+
+    // Kelvin to Fahrenheit
+    if(finalUnit === 'F') {
+        return ((startingTemp - 273.15) * (9/5) + 32);
+    }
+    // Kelvin to Celsius
+    else {
+        return (startingTemp - 273.15);
+    }
+
+}
+
+// export functions to be used in other programs
+module.exports = { getInput, tempConversion}
