43dcb824216a4bc62fb1e88f858e3e2b3bea5d0d
Zayon Nieves <zayon.nieves1@gmail.com>
2022-12-09 20:50:37 -0500

    corrected formatting for output

--- tempConverter.js
+++ tempConverter.js
@@ -1,5 +1,6 @@
 // import module
-import * as Functions from "./functions"
+const {getInput} = require('./functions.js');
+const {tempConversion} = require('./functions.js');
 
 
 // Step #1: Explain rules to the user
@@ -18,15 +19,16 @@ If any inputs are invalid, the program will notify and allow you to retry a new 
 // Step #2: variables for inputs (data we are taking in)
 
 // starting temperature (number and unit)
-let startingTemp = Functions.getInput('What is the starting temperature? ');
-let startingUnit = Functions.getInput('What is the starting unit? ');
+let startingTemp = getInput('What is the starting temperature? ');
+let startingUnit = getInput('What is the starting unit? ');
 
 // final temperature (only unit, final number is an output)
-let finalUnit = Functions.getInput('What is the final unit? ');
+let finalUnit = getInput('What is the final unit? ');
 
 // Step #4: Process the input (initial --> final). Produce an output.
 // Step #5: Display output to the user
 console.log(`
-The starting temperature was: ${String(startingTemp) + startingUnit}
+The starting temperature was: ${String(startingTemp) + ' ' + startingUnit}
+
 Converting to ${finalUnit}
-The final temperature is: ${Functions.tempConversion(startingTemp, startingUnit, finalUnit)}`);
+The final temperature is: ${tempConversion(startingTemp, startingUnit, finalUnit) + ' ' + finalUnit}`);
